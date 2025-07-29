ae41c500e0e24ff29d27e4a5cb061cb2940a6279
R112007 <1828476771@qq.com>
2025-07-29 11:42:48 +0800

    sb

---
+++ src/hf/HFMain.java
@@ -0,0 +1,30 @@
+package hf;
+
+import arc.Events;
+import arc.util.Log;
+import hf.ui.dialogs.SCResearchDialog;
+import mindustry.Vars;
+import mindustry.game.EventType.Trigger;
+import mindustry.mod.Mod;
+
+public class HFMain extends Mod {
+  @Override
+  public void loadContent() {
+  }
+
+  @Override
+  public void init() {
+    Log.info("loaded hf init");
+      Events.on(mindustry.game.EventType.ClientLoadEvent.class, (e) -> {
+        SCResearchDialog res = new SCResearchDialog();
+        Events.run(Trigger.update, () -> {
+          if (Vars.ui.research.isShown()) {
+            Vars.ui.research.hide();
+            if (!res.isShown()) {
+              res.show();
+            }
+          }
+        });
+      });
+  }
+}
